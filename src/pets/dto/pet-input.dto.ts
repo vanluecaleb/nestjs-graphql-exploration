5d688925a5b291f9151b56356c4ed443c61690da
Caleb Van Lue <vanluecaleb@outlook.com>
2025-03-24 13:32:29 -0400

    Add type validation to input

--- src/pets/dto/pet-input.dto.ts
+++ src/pets/dto/pet-input.dto.ts
@@ -1,10 +1,13 @@
 import { Field, InputType } from "@nestjs/graphql";
+import { IsAlpha } from "class-validator";
 
 @InputType()
 export class PetInput {
+    @IsAlpha()
     @Field()
     name: string;
 
+    @IsAlpha()
     @Field({ nullable: true })
     family?: string;
 }
